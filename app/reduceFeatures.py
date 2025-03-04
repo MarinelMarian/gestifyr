b3c4fe3ea3183da61aec5fa40f2d9cc874edf28f
tudorculda <tudor.culda@gmail.com>
2025-03-04 22:00:58 +0200

    minor cleanup

--- app/reduceFeatures.py
+++ app/reduceFeatures.py
@@ -1,65 +1,21 @@
 import os
 import csv
 import pandas as pd
-import tools as t
-from mediapipe_extract import get_distance_between_corner_eyes
+from tools import clear_terminal 
 import numpy as np
-import cv2
+from videoProcessingTools import points_to_extract, get_angles
 
-t.clear_terminal()
+clear_terminal()
 # Define input and output folders
 input_folder = (
-    "app/forTesting/processed"  # Change to your folder containing CSV files
+    "app/samples3/processed"  # Change to your folder containing CSV files
 )
-input_folder_raw = "app/forTesting/raw"
-output_folder = "app/forTesting/processed_trimmed"  # Folder to save processed files
+input_folder_raw = "app/samples3/raw"
+output_folder = "app/samples3/processed_trimmed"  # Folder to save processed files
 
 # Ensure output folder exists
 os.makedirs(output_folder, exist_ok=True)
 
-
-points_to_extract = [
-    4,
-    5,
-    25,
-]  # zero position indexs, browOuterUpLeft, browOuterUpRight, jawOpen
-points_to_get_angles = [1, 61, 291, 33, 263, 199]
-img_w, img_h = 1980, 1080
-
-
-def getAngles(result_features):
-    sublist = []
-    for point_idx in points_to_get_angles:
-        base_index = (point_idx - 1) * 3  # Each point has x, y, z coordinates
-        sublist.append(result_features[base_index : base_index + 3])
-    face_3d = [[int(e[0] * img_w), int(e[1] * img_h), e[2]] for e in sublist]
-    face_3d = np.array(face_3d, dtype=np.float64)
-    face_2d = np.array(face_3d[:, 0:2], dtype=np.float64)
-    focal_length = 1 * img_w
-
-    cam_matrix = np.array(
-        [[focal_length, 0, img_h / 2], [0, focal_length, img_w / 2], [0, 0, 1]]
-    )
-
-    # The distortion parameters
-    dist_matrix = np.zeros((4, 1), dtype=np.float64)
-
-    # Solve PnP
-    success, rot_vec, trans_vec = cv2.solvePnP(
-        face_3d, face_2d, cam_matrix, dist_matrix
-    )
-
-    # Get rotational matrix
-    rmat, jac = cv2.Rodrigues(rot_vec)
-
-    # Get angles
-    angles, mtxR, mtxQ, Qx, Qy, Qz = cv2.RQDecomp3x3(rmat)
-
-    # Get the y rotation degree
-    x = angles[0] * 360
-    y = angles[1] * 360
-    z = angles[2] * 360
-    return x, y
 
 
 # Process each CSV file in the input folder
@@ -77,8 +33,7 @@ for filename in os.listdir(input_folder):
         extracted_data = []
         for pf, rf in zip(processedFeatures.values, rawFeatures.values):
             extracted_row = [pf[i] for i in points_to_extract]
-            extracted_row.append(get_distance_between_corner_eyes(np.array(rf)))
-            x, y = getAngles(rf)
+            x, y = get_angles(rf)
             extracted_row.append(x)
             extracted_row.append(y)
 
@@ -100,9 +55,12 @@ for filename in os.listdir(input_folder):
                 "browOuterUpLeft",
                 "browOuterUpRight",
                 "jawOpen",
-                "distanceBetweenCornerEyes",
-                "verticalAngle",
-                "horizontalAngle",
+                "eyeBlinkLeft",
+                "eyeBlinkRight",
+                "smileLeft",
+                "smileRight",
+                "angleUpDown",
+                "angleLeftRight",
             ]
             writer.writerow(header)
 
