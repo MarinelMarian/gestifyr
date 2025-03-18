88a5e87b29f1586ff28f66d6f69b78c4230d9f47
Marinel Marian <Marinel.Marian@endava.com>
2025-03-18 12:17:05 +0200

    Refactor icon path definitions to use environment variables for better portability

--- app/icons.py
+++ app/icons.py
@@ -1,15 +1,21 @@
+import os
 import cv2
 from videoProcessingTools import overlayBar, overlayRoundedSquare
 import numpy as np
+from dotenv import load_dotenv
+
+load_dotenv()
+BASE_PATH = os.getenv("BASE_PATH")
+APP_REL_PATH = os.getenv("APP_REL_PATH")
 
 # Load the PNG icon with transparency
-icon_path_raise_eyebrow = "/Users/tudormarcel.culda/Work/python/inovationLab2024/github/gestifyr/app/images/raise_eyebrow.png"  # Change this to your PNG file
-icon_path_open_mouth = "/Users/tudormarcel.culda/Work/python/inovationLab2024/github/gestifyr/app/images/mouth_open.png"  # Change this to your PNG file
-icon_path_nod = "/Users/tudormarcel.culda/Work/python/inovationLab2024/github/gestifyr/app/images/nod.png"  # Change this to your PNG file
-icon_path_shake = "/Users/tudormarcel.culda/Work/python/inovationLab2024/github/gestifyr/app/images/shake.png"  # Change this to your PNG file
-icon_path_smile = "/Users/tudormarcel.culda/Work/python/inovationLab2024/github/gestifyr/app/images/smile.png"  # Change this to your PNG file
-icon_path_eyes_shut = "/Users/tudormarcel.culda/Work/python/inovationLab2024/github/gestifyr/app/images/eyes_shut.png"  # Change this to your PNG file
-icon_path_none = "/Users/tudormarcel.culda/Work/python/inovationLab2024/github/gestifyr/app/images/none.png"  # Change this to your PNG file
+icon_path_raise_eyebrow = f"{BASE_PATH}{APP_REL_PATH}images/raise_eyebrow.png"  # Change this to your PNG file
+icon_path_open_mouth = f"{BASE_PATH}{APP_REL_PATH}images/mouth_open.png"  # Change this to your PNG file
+icon_path_nod = f"{BASE_PATH}{APP_REL_PATH}images/nod.png"  # Change this to your PNG file
+icon_path_shake = f"{BASE_PATH}{APP_REL_PATH}images/shake.png"  # Change this to your PNG file
+icon_path_smile = f"{BASE_PATH}{APP_REL_PATH}images/smile.png"  # Change this to your PNG file
+icon_path_eyes_shut = f"{BASE_PATH}{APP_REL_PATH}images/eyes_shut.png"  # Change this to your PNG file
+icon_path_none = f"{BASE_PATH}{APP_REL_PATH}images/none.png"  # Change this to your PNG file
 
 icon_raise_eyebrows = cv2.imread(icon_path_raise_eyebrow, cv2.IMREAD_UNCHANGED)
 icon_open_mouth = cv2.imread(icon_path_open_mouth, cv2.IMREAD_UNCHANGED)
